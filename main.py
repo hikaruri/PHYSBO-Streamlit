dadd03690b9b4ef12b49500df2dad8c1c091a927
hikaruri <hikahikahikaru33@gmail.com>
2021-06-25 03:00:23 +0900

    Update plot

--- main.py
+++ main.py
@@ -16,24 +16,6 @@ def simulator(actions: int) -> float:
     return fx
 
 
-def plotfig(policy, X, Figname):
-    mean = policy.get_post_fmean(X)
-    var = policy.get_post_fcov(X)
-    std = np.sqrt(var)
-
-    x = X[:, 0]
-    fig, ax = plt.subplots()
-    ax.plot(x, mean)
-    ax.fill_between(x, (mean-std), (mean+std), color='b', alpha=.1)
-    ax.scatter(alpha_action, fx_action)
-    x1 = np.arange(0, 5, 0.01)
-    y1 = 0.1 * (2 * x1 - 1) * (x1 - 3) * (x1 - 5)
-    plt.plot(x1, y1, color='#ff4500')
-    st.pyplot(fig)
-    time.sleep(0.5)
-    # fig.savefig(Figname)
-
-
 if __name__ == '__main__':
     # Make a set of candidates, test_X
     window_num = 10001
@@ -49,12 +31,28 @@ if __name__ == '__main__':
     policy.random_search(max_num_probes=1, simulator=simulator)
     # policy.bayes_search(max_num_probes=10, simulator=simulator,
     #                     score="EI", interval=1, num_rand_basis=500)
+    plot_area = st.empty()
+    fig = plt.figure()
+    ax = fig.add_subplot()
 
-    for i in range(3):
+    for i in range(5):
         policy.bayes_search(max_num_probes=1, simulator=simulator, score="EI",
                             interval=1, num_rand_basis=i)
-        Figname = "BO_" + str(i) + ".png"
-        plotfig(policy, alpha_val, Figname)
+        ax.clear()
+        mean = policy.get_post_fmean(alpha_val)
+        var = policy.get_post_fcov(alpha_val)
+        std = np.sqrt(var)
+        x = alpha_val[:, 0]
+        ax.fill_between(x, (mean-std), (mean+std), color='b', alpha=.1)
+        ax.scatter(alpha_action, fx_action)
+        x1 = np.arange(0, 5, 0.01)
+        y1 = 0.1 * (2 * x1 - 1) * (x1 - 3) * (x1 - 5)
+        plt.plot(x1, y1, color='#ff4500')
+        # グラフを描画し直す
+        ax.plot(x, mean)
+        # プレースホルダに書き出す
+        plot_area.pyplot(fig)
+        time.sleep(3)
 
     # score = policy.get_score(mode="EI", xs=test_X)
     best_fx, best_actions = policy.history.export_sequence_best_fx()
