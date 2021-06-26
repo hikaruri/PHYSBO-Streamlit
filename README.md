8943d831de81f69ff530b335160539c9a926e108
sawahata <sawahata@sawahatanoMacBook-Pro.local>
2021-06-26 20:55:04 +0900

    Update

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # PHYSBO-Streamlit
 Web app. of the Bayesian optimization
-
+# Requirement
+```
+watchdog
+streamlit
+matplotlib
+watchdog
+click
+physbo
+```
 ## Ref.
 - [Streamlit Tutorial](https://docs.streamlit.io/en/stable/index.html)
 - [Python: Streamlit を使って手早く WebUI 付きのプロトタイプを作る](https://blog.amedama.jp/entry/streamlit-tutorial)
--- main.py
+++ main.py
@@ -29,9 +29,9 @@ if __name__ == '__main__':
     st.sidebar.title('Setting')
     One_Param_Func = st.sidebar.text_input(
                     label='Function',
-                    value='0.1 * (2 * x - 1) * (x - 3) * (x - 5)')
-    Rand_Num = st.sidebar.number_input('Random Search Num：', 1, 100, 1)
-    Bayz_Num = st.sidebar.number_input('Bayesian Opt. Num：', 1, 100, 5)
+                    value='np.sin(3 * x) + np.sin(x) - 0.1 * x')
+    Rand_Num = st.sidebar.number_input('Random Search Num：', 1, 100, 2)
+    Bayz_Num = st.sidebar.number_input('Bayesian Opt. Num：', 1, 100, 20)
 
     window_num = st.sidebar.number_input('X_Window Num：', 10001)
     alpha_min = st.sidebar.number_input('Xmin：', -100.0, 100.0, 0.0)
@@ -55,11 +55,14 @@ if __name__ == '__main__':
     if st.sidebar.button('start'):
         placeholder = st.empty()
         policy = physbo.search.discrete.policy(test_X=alpha_val)
+        policy.set_seed( 0 )
         policy.random_search(max_num_probes=Rand_Num, simulator=simulator)
         best_fx, best_actions = policy.history.export_sequence_best_fx()
         with placeholder:
             st.write(
                 f"best_fx: {best_fx[-1]} at {alpha_val[best_actions[-1], :]}")
+        ax.scatter(alpha_action, fx_action)
+        plot_area.pyplot(fig)
 
         for i in range(Bayz_Num):
             policy.bayes_search(max_num_probes=1, simulator=simulator,
