404c20942e7f6a407a8c24e2b7868f1bb9e0d41d
hikaruri <hikahikahikaru33@gmail.com>
2021-06-28 01:49:29 +0900

    Update

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 import physbo
 import time
+import re
 
 
 def function(One_Param_Func: str, x: float) -> float:
-    fx = eval(One_Param_Func)
+    # One_Param_Func is len < 30 and only include x
+    fx = eval(One_Param_Func) 
     return fx
 
 
@@ -20,13 +22,24 @@ if __name__ == '__main__':
 
     st.title('PHYSBO Simulation')
     st.sidebar.title('Setting')
+    
+    Func_Flag = True
     One_Param_Func = st.sidebar.text_input(
                     label='Function',
-                    value='np.sin(3 * x) + np.sin(x) - 0.1 * x')
-    Rand_Num = st.sidebar.number_input('Random Search Num：', 1, 100, 5)
-    Bayz_Num = st.sidebar.number_input('Bayesian Opt. Num：', 1, 100, 10)
+                    value='0.1*(2*x-1)*(x-3)*(x-5)')
 
-    window_num = st.sidebar.number_input('X_Window Num：', 10001)
+    Func_Flag = not(bool(re.search(r'[a-wyzA-WYZ]', One_Param_Func)))
+    if len(One_Param_Func) > 30:
+        Func_Flag = False
+    
+    if not Func_Flag:
+        st.warning('You can only use the parameter x')
+
+    
+    Rand_Num = st.sidebar.number_input('Random Search Num：', 1, 20, 5)
+    Bayz_Num = st.sidebar.number_input('Bayesian Opt. Num：', 1, 20, 10)
+
+    window_num = st.sidebar.number_input('X_Window Num：', 1, 20001, 10001)
     alpha_min = st.sidebar.number_input('Xmin：', -100.0, 100.0, 0.0)
     alpha_max = st.sidebar.number_input('Xmax：', -100.0, 100.0, 5.0)
     graph_mergin = st.sidebar.number_input('Mergin val.：', 0.0, 100.0, 0.5)
@@ -34,17 +47,20 @@ if __name__ == '__main__':
     alpha_val = np.linspace(alpha_min, alpha_max,
                             window_num).reshape(window_num, 1)
 
+
     plot_area = st.empty()
     fig = plt.figure()
     ax = fig.add_subplot()
     x1 = np.arange(alpha_min, alpha_max, (alpha_max-alpha_min)/window_num)
-    y1 = function(One_Param_Func, x1)
-    plt.plot(x1, y1, color='#ff4500')
-    plt.xlim([alpha_min, alpha_max])
-    plt.ylim([min(y1)-graph_mergin, max(y1)+graph_mergin])
-    plot_area.pyplot(fig)
+    
+    if Func_Flag:
+        y1 = function(One_Param_Func, x1)
+        plt.plot(x1, y1, color='#ff4500')
+        plt.xlim([alpha_min, alpha_max])
+        plt.ylim([min(y1)-graph_mergin, max(y1)+graph_mergin])
+        plot_area.pyplot(fig)
 
-    if st.sidebar.button('start'):
+    if st.sidebar.button('start') and Func_Flag:
         placeholder = st.empty()
         policy = physbo.search.discrete.policy(test_X=alpha_val)
         policy.set_seed(int(time.time()))
